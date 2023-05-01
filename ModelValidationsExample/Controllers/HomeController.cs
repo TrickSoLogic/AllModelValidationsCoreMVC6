03796ad0c7999011e2ae7c4ea962ebb348e83446
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 21:59:42 +0530

    Add project files.

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AllModelValidationsCoreMVC6.Models;
+
+namespace AllModelValidationsCoreMVC6.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("register")]
+    public IActionResult Index(Person person)
+    {
+      if (!ModelState.IsValid)
+      {
+        string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+  
+        return BadRequest(errors);
+      }
+
+      return Content($"{person}");
+    }
+  }
+}
